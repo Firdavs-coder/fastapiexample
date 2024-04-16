4a3b02e76b4603a0df40afa9208d0de57840813f
Firdavs_Coder <shodiyev-f@mail.ru>
2024-04-16 19:21:02 +0500

    PyLint was added

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+"This is the main runner file of the project"
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,9 +7,13 @@ app = FastAPI()
 
 @app.get("/hello")
 async def hello():
+    "This just returns greeting to developers"
+
     return {"hello": "Hello Developers 4"}
 
 
 @app.get("/status")
-async def hello():
+async def status():
+    "This is a status of the project"
+
     return {"status": 401}
